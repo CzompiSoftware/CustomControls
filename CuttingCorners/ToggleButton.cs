626c5ed0a2cad2407594d578dbe071ef7dc8ec79
Dávid Czompó <czompi@czompi.hu>
2020-12-29 22:34:11 +0100

    1.3.5.3

---
+++ CuttingCorners/ToggleButton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace hu.czompisoftware.customcontrols.CuttingCorners
+{
+    public class ToggleButton : System.Windows.Controls.Primitives.ToggleButton
+    {
+        static ToggleButton()
+        {
+        }
+
+        #region HoverBackground property
+        private static readonly DependencyProperty HoverBackgroundProperty =
+            DependencyProperty.Register(
+                "HoverBackground", typeof(Brush), typeof(ToggleButton),
+                new PropertyMetadata(default(Brush)));
+
+        public Brush? HoverBackground
+        {
+            get => (Brush?)GetValue(HoverBackgroundProperty);
+            set => SetValue(HoverBackgroundProperty, value);
+        }
+        #endregion
+
+    }
+}
